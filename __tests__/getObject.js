4a42708651e857272294cac313f0d4a3a95ba807
Dmitry Ovsyanko <do_@pochta.ru>
2023-01-15 13:51:54 +0300

    getObject

---
+++ __tests__/getObject.js
@@ -0,0 +1,161 @@
+const EventEmitter = require ('events')
+const {DbClientPg, DbPoolPg} = require ('..'), {normalizeSQL} = DbClientPg
+
+const job = new EventEmitter ()
+job.uuid = '00000000-0000-0000-0000-000000000000'
+job.logger = {log: ({message, level}) => {if (false) console.log (level + ' ' + message)}}
+
+const pool = new DbPoolPg ({
+	db: {
+		connectionString: process.env.CONNECTION_STRING,
+	},
+})
+
+afterAll(async () => {
+
+	await pool.pool.end ()
+
+})
+
+test ('e7707', async () => {
+	
+	try {
+	
+		var db = await pool.toSet (job, 'db')
+
+		const s = await db.getObject ('...')	
+
+	}
+	catch (x) {
+
+		expect (x).toBeInstanceOf (Error)
+
+	}
+	finally {
+
+		await db.release ()
+
+	}
+	
+})
+
+test ('sequence', async () => {
+	
+	try {
+	
+		var db = await pool.toSet (job, 'db')
+
+		const o = await db.getObject ('SELECT * FROM generate_series (?::int, ?) id', [1, 10])
+
+		expect (o).toStrictEqual ({id: 1})
+
+	}
+	finally {
+
+		await db.release ()
+
+	}
+	
+})
+
+test ('1-to-1', async () => {
+	
+	try {
+	
+		var db = await pool.toSet (job, 'db')
+
+		const o = await db.getObject ('SELECT 1 id', [])
+
+		expect (o).toStrictEqual ({id: 1})
+
+	}
+	finally {
+
+		await db.release ()
+
+	}
+	
+})
+
+test ('1 array', async () => {
+	
+	try {
+	
+		var db = await pool.toSet (job, 'db')
+
+		const o = await db.getObject ('SELECT 1 id', [], {rowMode: 'array'})
+
+		expect (o).toStrictEqual ([1])
+
+	}
+	finally {
+
+		await db.release ()
+
+	}
+	
+})
+
+test ('default', async () => {
+	
+	try {
+	
+		var db = await pool.toSet (job, 'db')
+
+		const o = await db.getObject ('SELECT 1 id WHERE false', [])
+
+		expect (o).toStrictEqual ({})
+
+	}
+	finally {
+
+		await db.release ()
+
+	}
+	
+})
+
+test ('custom default', async () => {
+	
+	try {
+	
+		var db = await pool.toSet (job, 'db')
+		
+		const DEF = {id: -1}
+
+		const o = await db.getObject ('SELECT 1 id WHERE false', [], {ifNull: DEF})
+
+		expect (o).toBe (DEF)
+
+	}
+	finally {
+
+		await db.release ()
+
+	}
+	
+})
+
+test ('custom error', async () => {
+	
+	const DEF = new Error ('Not Found')
+
+	try {
+	
+		var db = await pool.toSet (job, 'db')
+		
+		const o = await db.getObject ('SELECT 1 id WHERE false', [], {ifNull: DEF})
+
+	}
+	catch (x) {
+
+		expect (x.cause).toBe (DEF)
+
+	}
+	finally {
+
+		await db.release ()
+
+	}
+	
+})
