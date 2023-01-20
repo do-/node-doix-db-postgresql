a2dbb3de2502a743eff07d4bd3011a95611460cf
Dmitry Ovsyanko <do_@pochta.ru>
2023-01-20 09:29:53 +0300

    DbLangPg extracted

--- __tests__/normalizeSQL.js
+++ __tests__/normalizeSQL.js
@@ -1,20 +1,20 @@
-const {DbClientPg} = require ('..'), {normalizeSQL} = DbClientPg
+const {DbLangPg} = require ('..'), lang = new DbLangPg ()
 
 test ('normalizeSQL', () => {
 
-	expect (normalizeSQL ('SELECT 1-1/2--')).toBe ('SELECT 1-1/2')
+	expect (lang.normalizeSQL ('SELECT 1-1/2--')).toBe ('SELECT 1-1/2')
 
-	expect (normalizeSQL ('SELECT * FROM t WHERE id = ?')).toBe ('SELECT * FROM t WHERE id = $1')
+	expect (lang.normalizeSQL ('SELECT * FROM t WHERE id = ?')).toBe ('SELECT * FROM t WHERE id = $1')
 	
-	expect (normalizeSQL ('SELECT * FROM t WHERE id = ? AND label LIKE ?')).toBe ('SELECT * FROM t WHERE id = $1 AND label LIKE $2')
+	expect (lang.normalizeSQL ('SELECT * FROM t WHERE id = ? AND label LIKE ?')).toBe ('SELECT * FROM t WHERE id = $1 AND label LIKE $2')
 
-	expect (normalizeSQL ('SELECT * FROM t WHERE id::jsonb ? ? AND label LIKE ?')).toBe ('SELECT * FROM t WHERE id::jsonb ? $1 AND label LIKE $2')
+	expect (lang.normalizeSQL ('SELECT * FROM t WHERE id::jsonb ? ? AND label LIKE ?')).toBe ('SELECT * FROM t WHERE id::jsonb ? $1 AND label LIKE $2')
 
-	expect (normalizeSQL ("SELECT * FROM t WHERE id = ? AND label='Don''t you know?'")).toBe ("SELECT * FROM t WHERE id = $1 AND label='Don''t you know?'")
+	expect (lang.normalizeSQL ("SELECT * FROM t WHERE id = ? AND label='Don''t you know?'")).toBe ("SELECT * FROM t WHERE id = $1 AND label='Don''t you know?'")
 
-	expect (normalizeSQL ('SELECT * FROM t /*What /*the he// is*/t?*/ WHERE id = ?')).toBe ('SELECT * FROM t  WHERE id = $1')
+	expect (lang.normalizeSQL ('SELECT * FROM t /*What /*the he// is*/t?*/ WHERE id = ?')).toBe ('SELECT * FROM t  WHERE id = $1')
 
-	expect (normalizeSQL (`
+	expect (lang.normalizeSQL (`
 		SELECT
 			id
 --			, label ???
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
 	DbClientPg: require ('./lib/DbClientPg.js'),
 	DbPoolPg: require ('./lib/DbPoolPg.js'),
+	DbLangPg: require ('./lib/DbLangPg.js'),
 }
