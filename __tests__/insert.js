ee11c5479f1f36e16486df75ed50a32c395f7a22
Dmitry Ovsyanko <do_@pochta.ru>
2024-03-24 14:45:35 +0300

    insertRecord: return boolean when onlyIfMissing

--- __tests__/insert.js
+++ __tests__/insert.js
@@ -1,5 +1,5 @@
 const Path = require ('path')
-const {DbModel} = require ('doix-db')
+const {DbModel, DbCall} = require ('doix-db')
 const MockJob = require ('./lib/MockJob.js'), job = new MockJob ()
 const {DbPoolPg} = require ('..')
 
@@ -32,13 +32,19 @@ test ('basic', async () => {
 		await db.do ('DROP TABLE IF EXISTS tb_3')
 		await db.do ('CREATE TABLE tb_3 (id int PRIMARY KEY, label text)')
 
-		await db.insert ('tb_3', {id: 1, label: 'user'})
+		const r1 = await db.insert ('tb_3', {id: 1, label: 'user'})
+
+		expect (r1.constructor.name).toBe ('DbCall')		
 		expect (await db.getArray ('SELECT * FROM tb_3')).toStrictEqual ([{id: 1, label: 'user'}])
 
 		await expect (db.insert ('tb_3', {id: 1, label: 'admin'})).rejects.toThrow ()
 
-		await db.insert ('tb_3', {id: 1, label: 'admin'}, {onlyIfMissing: true})
+		const r0 = await db.insert ('tb_3', {id: 1, label: 'admin'}, {onlyIfMissing: true})
 		expect (await db.getArray ('SELECT * FROM tb_3')).toStrictEqual ([{id: 1, label: 'user'}])
+		expect (r0).toBe (false)
+
+		const r2 = await db.insert ('tb_3', {id: 2, label: 'alien'}, {onlyIfMissing: true})
+		expect (r2).toBe (true)
 
 	}
 	
