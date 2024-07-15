806bf29452bcd3d0b0cd7cd4e549cdc0f30e5a34
Dmitry Ovsyanko <do_@pochta.ru>
2024-07-15 09:40:02 +0300

    DbChannelPg -getQueue

--- __tests__/DbChannelPg.js
+++ __tests__/DbChannelPg.js
@@ -1,12 +1,10 @@
 const Path = require ('path')
 const {Application, ResourcePool} = require ('doix')
-const {DbModel} = require ('doix-db')
-const {DbChannelPg, DbListenerPg, DbPoolPg, DbQueuePg} = require ('..')
+const {DbChannelPg, DbPoolPg} = require ('..')
 
 const logger = {log: _ => {}}//new ConsoleLogger ()
 const modules = {dir: {root: Path.join (__dirname, 'data', 'root3')}}
 
-const schemaName = 'doix_test_db_4'
 const db = {
 	connectionString: process.env.CONNECTION_STRING,
 }
@@ -25,42 +23,5 @@ test ('bad', () => {
 
 	const ch = new DbChannelPg (app, {name: 'hotline'})
 	ch.router = {}
-
-})
-
-test ('getQueue', async () => {
-
-	app.pools.get ('db2').wrapper = Object
-
-	const pool = app.pools.get ('db')
-
-	try {
-
-		const model = new DbModel ({
-			src: {
-				schemaName,
-				root: Path.join (__dirname, 'data', 'root4')
-			},
-			db: pool
-		})
-	
-		model.loadModules ()
-	
-		const dbl = new DbListenerPg ({db, logger})
-	
-		const ch = new DbChannelPg (app, {name: 'hotline'})
-	
-		dbl.add (ch)
-	
-		expect (ch.router).toBe (dbl)
-		expect (ch.getQueue ({})).toBeUndefined ()
-		expect (ch.getQueue ({payload: 'q_1'})).toBeInstanceOf (DbQueuePg)
-
-	}
-	finally {
-
-		await pool.pool.end ()
-
-	}
 
 })
