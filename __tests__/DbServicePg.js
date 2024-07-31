9baebb62d125fef570bc210bf7578548aed3a213
Dmitry Ovsyanko <do_@pochta.ru>
2024-07-31 08:30:55 +0300

    ver++

--- __tests__/DbServicePg.js
+++ __tests__/DbServicePg.js
@@ -1,8 +1,21 @@
-const {DbServicePg, DbNotificationPg} = require ('..')
+const Path = require ('path')
+const {DbServicePg, DbNotificationPg, DbPoolPg} = require ('..')
+const {Application} = require ('doix')
+
+const logger = 
+{log: _ => {}}
+//new ConsoleLogger ()
+const modules = {dir: {root: Path.join (__dirname, 'data', 'root3')}}
+	
+const db = {
+	connectionString: process.env.CONNECTION_STRING,
+}
+	
+const app = new Application ({modules, logger, pools: {db: new DbPoolPg ({db, logger})}})
 
 test ('test', async () => {
 
-	const svc = new DbServicePg ()
+	const svc = new DbServicePg (app)
 
 	expect (svc.test (new DbNotificationPg({payload: 'some_queue'}))).toBe (false)
 	expect (svc.test (new DbNotificationPg({payload: '{'}))).toBe (false)
